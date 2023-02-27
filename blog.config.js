59372ffa3370869e4a40ec463d9cd3e30f8bf51e
snipersteve <sfl05@163.com>
2023-02-28 01:24:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'STEVE',
-  author: 'ste',
+  author: 'SHOW',
   email: 'sfl05@163.com',
   link: 'https://show.vercel.app',
   description: 'This gonna be an awesome website.',
@@ -13,11 +13,11 @@ const BLOG = {
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: false,
-  showAbout: true,
+  showAbout: false,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/snipersteve',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
